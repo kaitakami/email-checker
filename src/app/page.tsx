56829807bf158e3dca170287604f1c2941a17ada
Kai Takami <kaitakami@protonmail.com>
2023-05-10 14:30:30 -0600

    Create user interface

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,86 @@
-import Image from 'next/image'
+"use client"
+
+import { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { checkWords } from "../utils/checkWords";
+import type { iWord } from "../utils/words";
+
+type Inputs = {
+  subject: string,
+  content: string,
+};
+
+interface NegativeWords {
+  badWords: iWord[],
+  spamWords: string[]
+}
 
 export default function Home() {
+  const [negativeWords, setNegativeWords] = useState<NegativeWords>();
+  const { register, handleSubmit, formState: { errors, isSubmitSuccessful } } = useForm<Inputs>();
+  console.log(negativeWords)
+  const onSubmit: SubmitHandler<Inputs> = data => setNegativeWords(checkWords(`${data.content.toLowerCase()} \n ${data.subject.toLowerCase()}`));
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main className="flex flex-col items-center justify-center max-w-6xl min-h-screen p-4 mx-auto gap-7 md:p-20">
+      <section className='w-full p-2 py-5 border rounded-lg bg-zinc-100 dark:bg-zinc-800 border-zinc-300 dark:border-zinc-600'>
+        <form className="flex flex-col gap-2" onSubmit={handleSubmit(onSubmit)}>
+          <h2 className="text-xl font-semibold">Email</h2>
+          <input
+            placeholder="Subject"
+            className="bg-zinc-50 border border-zinc-300 text-zinc-900 text-sm rounded-lg focus:ring-zinc-600 focus:border-zinc-600 block w-full p-2.5 dark:bg-zinc-700 dark:border-zinc-600 dark:placeholder-zinc-400 dark:text-white dark:focus:ring-zinc-500 dark:focus:border-zinc-500"
+            {...register("subject", { required: true })}
+          />
+          {errors.content && <span className="block text-sm font-medium text-center text-red-400/80">This field is required</span>}
+          <textarea
+            placeholder="Content"
+            className="block p-2.5 w-full text-sm text-zinc-900 bg-zinc-50 rounded-lg border border-zinc-300 focus:ring-zinc-500 focus:border-zinc-500 dark:bg-zinc-700 dark:border-zinc-600 dark:placeholder-zinc-400 dark:text-white dark:focus:ring-zinc-500 dark:focus:border-zinc-500 max-h-96 min-h-[10rem] sm:min-h-[15rem]"
+            {...register("content", { required: true })}
+          />
+          {errors.content && <span className="block text-sm font-medium text-center text-red-400/80">This field is required</span>}
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          <button type="submit" className="dark:text-white text-black border border-zinc-300 dark:border-zinc-600 bg-zinc-200 hover:bg-zinc-300 focus:ring-4 focus:outline-none focus:ring-zinc-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-zinc-600 dark:hover:bg-zinc-700 dark:focus:ring-zinc-800 transition-colors">
+            Check Email
+          </button>
+        </form>
+      </section>
+      <section className='w-full p-2 py-5 border rounded-lg bg-zinc-100 dark:bg-zinc-800 border-zinc-300 dark:border-zinc-600'>
+        <h2 className="text-xl font-semibold">Improvements</h2>
+        {isSubmitSuccessful && negativeWords ?
+          <NegativeWordsList badWords={negativeWords.badWords} spamWords={negativeWords.spamWords} />
+          : <div className="opacity-50">Start writing your email</div>}
+      </section>
     </main>
+  )
+}
+
+const NegativeWordsList = ({ badWords, spamWords }: NegativeWords) => {
+  if (!badWords.length && !spamWords.length) {
+    return (
+      <h3>
+        Your emails doesn&apos;t seem to have a negative word. Congrats :)
+      </h3>
+    )
+  }
+  return (
+    <>
+      {badWords.length > 0 && (
+        <>
+          <h3 className="pt-3 text-sm font-semibold">Bad words</h3>
+          <ul className="px-5">
+            {badWords.map(word => (<li className="list-disc" key={word.word}>
+
+            </li>))}
+          </ul>
+        </>
+      )}
+      {spamWords.length > 0 && (
+        <>
+          <h3 className="pt-3 text-sm font-semibold">Spam words</h3>
+          <ul className="px-5">
+            {spamWords.map(word => (<li className="list-disc" key={word}>{word}</li>))}
+          </ul>
+        </>
+      )}
+    </>
   )
 }
